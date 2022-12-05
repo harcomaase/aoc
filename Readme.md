b0562cea3771ded4bc60b711bd20331db9c8b96a
harcomaase <github@marco-haase.de>
2022-12-05 13:28:24 +0100

    2022-12-05 solution

--- Readme.md
+++ Readme.md
@@ -2,6 +2,7 @@
 
 ## 2022
 - rust all the way :)
+- created script to measure the execution time of all programs together, goal: minimize it :)
 
 ## 2021
 - again everything in rust, using either vscode or vim
---
+++ rs-22/src/bin/day5.rs
@@ -0,0 +1,62 @@
+use std::fs;
+
+use regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("../input/22/day5.txt").unwrap();
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    let stacks_quantity = (lines[0].chars().count() + 1) / 4;
+    let empty_line_number = lines
+        .iter()
+        .enumerate()
+        .find(|(_i, line)| line.trim().is_empty())
+        .map(|(i, _line)| i)
+        .unwrap();
+
+    let mut stacks = parse_stacks(stacks_quantity, &lines, empty_line_number);
+
+    let instruction_regex = Regex::new(r"move ([\d]+) from ([\d]+) to ([\d]+)").unwrap();
+
+    for line in &lines[(empty_line_number + 1)..] {
+        let captures = instruction_regex.captures(line).unwrap();
+        let quantity = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
+        let src = captures.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let dest = captures.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1;
+
+        for _i in 0..quantity {
+            let value = stacks[src].pop().unwrap();
+            stacks[dest].push(value);
+        }
+    }
+
+    for stack in stacks {
+        print!("{}", stack.last().unwrap());
+    }
+    println!()
+}
+
+fn parse_stacks(
+    stacks_quantity: usize,
+    lines: &Vec<&str>,
+    empty_line_number: usize,
+) -> Vec<Vec<char>> {
+    let mut stacks = vec![Vec::new(); stacks_quantity];
+    for line in &lines[..(empty_line_number - 1)] {
+        let chars: Vec<char> = line.chars().collect();
+        for (i, pos) in (1..(stacks_quantity * 4)).step_by(4).enumerate() {
+            let c = chars[pos];
+            match c {
+                ' ' => (),
+                _ => stacks[i].push(c),
+            }
+        }
+    }
+    let mut result = Vec::with_capacity(stacks.len());
+    for mut stack in stacks.into_iter() {
+        stack.reverse();
+        result.push(stack);
+    }
+    result
+}
