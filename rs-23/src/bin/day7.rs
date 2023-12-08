8b20e2734682a897e2ae6bd8573ff1ee4eb53c72
Haase Marco (HC/ENG-IOT) <ham2lol@>
2023-12-08 16:30:35 +0100

    continue day7

--- rs-23/src/bin/day7.rs
+++ rs-23/src/bin/day7.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fs};
 
-#[derive(PartialEq)]
+#[derive(Debug)]
 struct Hand {
     cards: Vec<char>,
 }
@@ -84,18 +84,24 @@ impl From<char> for Card {
     }
 }
 
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cards.eq(&other.cards)
+    }
+}
+
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         let o1 = HandType::from(self);
         let o2 = HandType::from(other);
 
-        Some(match o2.cmp(&o1) {
+        Some(match o1.cmp(&o2) {
             std::cmp::Ordering::Less => std::cmp::Ordering::Less,
             std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
             std::cmp::Ordering::Equal => {
                 // we need to compare hands card by card
                 for i in 0..self.cards.len() {
-                    match self.cards[i].cmp(&other.cards[i]) {
+                    match Card::from(self.cards[i]).cmp(&Card::from(other.cards[i])) {
                         std::cmp::Ordering::Equal => continue,
                         std::cmp::Ordering::Less => return Some(std::cmp::Ordering::Less),
                         std::cmp::Ordering::Greater => return Some(std::cmp::Ordering::Greater),
@@ -108,7 +114,32 @@ impl PartialOrd for Hand {
 }
 
 fn main() {
-    let _input = fs::read_to_string("../input/23/day7test.txt").unwrap();
+    let input = fs::read_to_string("../input/23/day7.txt").unwrap();
+
+    let mut hand_with_bids: Vec<(Hand, u64)> = input
+        .lines()
+        .map(|line| line.split_ascii_whitespace())
+        .map(|mut split| {
+            (
+                Hand {
+                    cards: split.next().unwrap().chars().collect(),
+                },
+                split.next().unwrap().parse::<u64>().unwrap(),
+            )
+        })
+        .collect();
+
+    hand_with_bids.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let total_winnings: u64 = hand_with_bids
+        .iter()
+        .enumerate()
+        .map(|(i, hwb)| (i as u64 + 1) * hwb.1)
+        .sum();
+    for hwb in hand_with_bids {
+        println!("{:?}",hwb);
+    }
+    println!("{total_winnings}");
 }
 
 #[cfg(test)]
@@ -123,13 +154,101 @@ mod tests {
         assert!(Card::from('2') < Card::from('3'));
         assert!(Card::from('4') > Card::from('3'));
 
+        assert!(Card::from('K') > Card::from('T'));
+
+        let five_of_a_kind = &Hand {
+            cards: vec!['2', '2', '2', '2', '2'],
+        };
+        let four_of_a_kind = &Hand {
+            cards: vec!['A', '2', '2', '2', '2'],
+        };
+        let three_of_a_kind = &Hand {
+            cards: vec!['A', 'K', '2', '2', '2'],
+        };
+        let three_of_a_kind2 = &Hand {
+            cards: vec!['A', '2', '2', 'K', '2'],
+        };
+        let full_house = &Hand {
+            cards: vec!['A', 'A', '2', '2', '2'],
+        };
+        let full_house2 = &Hand {
+            cards: vec!['A', 'A', 'A', '2', '2'],
+        };
+        let two_pair = &Hand {
+            cards: vec!['2', '2', '3', '3', '4'],
+        };
+        let two_pair2 = &Hand {
+            cards: vec!['A', '2', 'A', '2', '4'],
+        };
+        let one_pair = &Hand {
+            cards: vec!['A', 'A', '2', '3', '4'],
+        };
+        let one_pair2 = &Hand {
+            cards: vec!['A', 'K', '2', '3', 'A'],
+        };
+        let one_pair3 = &Hand {
+            cards: vec!['K', 'A', '2', '2', '4'],
+        };
+        let high_card = &Hand {
+            cards: vec!['2', '3', '4', '5', '6'],
+        };
+
+        assert_eq!(HandType::FiveOfAKind, HandType::from(five_of_a_kind));
+        assert_eq!(HandType::FourOfAKind, HandType::from(four_of_a_kind));
+        assert_eq!(HandType::FullHouse, HandType::from(full_house));
+        assert_eq!(HandType::FullHouse, HandType::from(full_house2));
+        assert_eq!(HandType::ThreeOfAKind, HandType::from(three_of_a_kind));
+        assert_eq!(HandType::ThreeOfAKind, HandType::from(three_of_a_kind2));
+        assert_eq!(HandType::TwoPair, HandType::from(two_pair));
+        assert_eq!(HandType::TwoPair, HandType::from(two_pair2));
+        assert_eq!(HandType::OnePair, HandType::from(one_pair));
+        assert_eq!(HandType::OnePair, HandType::from(one_pair2));
+        assert_eq!(HandType::OnePair, HandType::from(one_pair3));
         assert_eq!(
-            HandType::from(&Hand {
-                cards: vec!['2', '2', '2', '2', '2']
-            }),
-            HandType::from(&Hand {
-                cards: vec!['2', '2', '2', '2', '2']
-            })
+            HandType::HighCard(Card::from('6')),
+            HandType::from(high_card)
+        );
+
+        assert_eq!(
+            HandType::from(five_of_a_kind),
+            HandType::from(five_of_a_kind)
+        );
+
+        assert!(
+            Hand {
+                cards: vec!['T', 'T', 'T', 'T', '7']
+            } > Hand {
+                cards: vec!['T', 'T', 'T', '7', '7']
+            }
+        );
+        assert!(
+            Hand {
+                cards: vec!['T', 'T', 'T', '7', '6']
+            } > Hand {
+                cards: vec!['T', 'T', 'T', '6', '7']
+            }
+        );
+        assert!(
+            Hand {
+                cards: vec!['T', 'T', 'T', '7', '8']
+            } < Hand {
+                cards: vec!['T', 'T', 'T', '7', '9']
+            }
+        );
+        assert!(
+            Hand {
+                cards: vec!['T', 'T', 'T', '7', '9']
+            } == Hand {
+                cards: vec!['T', 'T', 'T', '7', '9']
+            }
+        );
+        println!("===");
+        assert!(
+            Hand {
+                cards: vec!['K', 'K', '6', '7', '7']
+            } > Hand {
+                cards: vec!['K', 'T', 'J', 'J', 'T']
+            }
         );
     }
 }
