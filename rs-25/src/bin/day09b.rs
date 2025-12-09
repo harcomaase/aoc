967bdcb5be86e1997fd9ea5f6419f3b3f21019ec
harcomaase <github@marco-haase.de>
2025-12-09 17:41:29 +0100

    feat: day 9, yes

--- rs-25/src/bin/day09b.rs
+++ rs-25/src/bin/day09b.rs
@@ -31,29 +31,66 @@ fn main() {
     // ... which leads to the conclusion, that one of the points marked as 'x'
     // is probably a corner of the rectangle we are looking for.
     // The plan:
-    //   - find the tiles that are marked as x, programmatically
-    //   - check for rectangles that have one of these tiles as corner
-    //   - implement simple collision checking - check for intersecion of lines
+    //   - (1) find the tiles that are marked as x, programmatically
+    //   - (2) check for rectangles that have one of these tiles as corner
+    //   - (3) implement simple collision checking - check for intersecion of lines
+    // (this is very un-generic, yes)
 
-    //TODO: below is part 1 code - update with above notes
+    // (1)
+    let mut hot_tile1 = None;
+    let mut hot_tile2 = None;
+    for i in 0..red_tiles.len() {
+        let t1 = red_tiles[i];
+        let t2_index = if i == red_tiles.len() - 1 { 0 } else { i + 1 };
+        let t2 = red_tiles[t2_index];
+
+        if (t1.0 - t2.0).abs() > 10000 {
+            match hot_tile1 {
+                Some(_) => {
+                    hot_tile2 = Some(t1);
+                    break;
+                }
+                None => {
+                    hot_tile1 = Some(t2);
+                }
+            }
+        }
+    }
+
+    let t1 = hot_tile1.unwrap();
+    let t2 = hot_tile2.unwrap();
+
+    // (2)
     let mut biggest_area = 0;
-    for i1 in 0..red_tiles.len() {
-        for i2 in (i1 + 1)..red_tiles.len() {
-            let area = ((red_tiles[i1].0 - red_tiles[i2].0).abs() + 1)
-                * ((red_tiles[i1].1 - red_tiles[i2].1).abs() + 1);
+    for tile1 in vec![t1, t2] {
+        'loopy: for i2 in 0..red_tiles.len() {
+            let tile2 = red_tiles[i2];
+            let area = ((tile1.0 - tile2.0).abs() + 1) * ((tile1.1 - tile2.1).abs() + 1);
+
+            // (3)
+            let x1 = tile1.0.min(tile2.0);
+            let x2 = tile1.0.max(tile2.0);
+            let y1 = tile1.1.min(tile2.1);
+            let y2 = tile1.1.max(tile2.1);
+
+            for t in &red_tiles {
+                if t.0 > x1 && t.0 < x2 && t.1 > y1 && t.1 < y2 {
+                    // this check has one fault: it does not properly take the inner
+                    // part of the circle into account. So we add another cheap check
+                    // afterwards
+                    continue 'loopy;
+                }
+            }
+            // cheap indeed, but works
+            let other_y = if tile1 == t1 { t2.1 } else { t1.1 };
+            if other_y > y1 && other_y < y2 {
+                continue 'loopy;
+            }
+
             if area > biggest_area {
                 biggest_area = area;
             }
         }
     }
     println!("{biggest_area}");
-
-    let mut i: usize = 0;
-    for _ in 0..100000 {
-        for _ in 0..100000 {
-            i += 1;
-        }
-    }
-
-    println!("{i}");
 }
